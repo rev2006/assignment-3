cd52acd7b16bc90186622a4fa2ac5b74bb502d11
Rev <revants@student.unimelb.edu.au>
2025-04-27 19:11:50 +1000

    working side panel

--- A3.js
+++ A3.js
@@ -1,12 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+//header
+
+//mobile side panel
+
+    const menuButton = document.getElementById('menu_button');
+    const sidePanel = document.getElementById('side_panel');
+    menuButton.addEventListener('click', () =>
+        sidePanel.classList.toggle('open'));
+
+    const closeMenu = document.getElementById('closeMenu');
+    closeMenu.addEventListener('click', () =>
+    sidePanel.classList.remove('open'));
+
+
+
+
+
+
 
 //home page 
 
 //scrolling
-const scrollLIT = document.getElementById("LIT_items");
+const scrollLIT = document.getElementById('LIT_items');
 const scrollAm = 300;
 document.getElementById("scroll_l").addEventListener("click", () => {
     scrollLIT.scrollBy({left: -scrollAm, behavior:"smooth"});
 });
 document.getElementById("scroll_r").addEventListener("click", () => {
     scrollLIT.scrollBy({left: scrollAm, behavior:"smooth"});
+});
+
+
+
+
+
+
+
+
+
 });
