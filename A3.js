6418327778a56d20748ff1bf49df40f4ed94f6be
Rev <revants@student.unimelb.edu.au>
2025-04-30 17:52:06 +1000

    minor changes

--- A3.js
+++ A3.js
@@ -18,61 +18,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-//home page 
+    //home page 
 
-//scrolling
+    //scrolling
 
-//scrolling homepage items
-const scrollLIT = document.getElementById('LIT_items');
-const scrollTS = document.getElementById('TS_items');
-const scrollAm = 300;
-const leftButtons = document.getElementsByClassName("scroll_button_l");
-const rightButtons = document.getElementsByClassName("scroll_button_r");
+    //scrolling homepage items
+    const scrollLIT = document.getElementById('LIT_items');
+    const scrollTS = document.getElementById('TS_items');
+    const scrollAm = 300;
+    const leftButtons = document.getElementsByClassName("scroll_button_l");
+    const rightButtons = document.getElementsByClassName("scroll_button_r");
 
-for (let i = 0; i < leftButtons.length; i++) {
-    leftButtons[i].addEventListener("click", () => {
-        if (i === 0) {
-            scrollLIT.scrollBy({left: -scrollAm, behavior: "smooth"});
-        } else {
-            scrollTS.scrollBy({left: -scrollAm, behavior: "smooth"});
+    for (let i = 0; i < leftButtons.length; i++) {
+        leftButtons[i].addEventListener("click", () => {
+            if (i === 0) {
+                scrollLIT.scrollBy({left: -scrollAm, behavior: "smooth"});
+            } else {
+                scrollTS.scrollBy({left: -scrollAm, behavior: "smooth"});
+            }
+        });
+    }
+    for (let i = 0; i < rightButtons.length; i++) {
+        rightButtons[i].addEventListener("click", () => {
+            if (i === 0) {
+                scrollLIT.scrollBy({left: scrollAm, behavior: "smooth"});
+            } else {
+                scrollTS.scrollBy({left: scrollAm, behavior: "smooth"});
+            }
+        });
+    }
+    //scrolling product page items
+    const scrollProd = document.getElementById('photos_list');
+    const scrollReviews = document.getElementById('reviews');
+    for (let i = 0; i < leftButtons.length; i++) {
+        leftButtons[i].addEventListener("click", () => {
+            if (i === 0) {
+                scrollProd.scrollBy({left: -scrollAm, behavior: "smooth"});
+            } else {
+                scrollReviews.scrollBy({left: -scrollAm, behavior: "smooth"});
+            }
+        });
+    }
+    for (let i = 0; i < rightButtons.length; i++) {
+        rightButtons[i].addEventListener("click", () => {
+            if (i === 0) {
+                scrollProd.scrollBy({left: scrollAm, behavior: "smooth"});
+            } else {
+                scrollReviews.scrollBy({left: scrollAm, behavior: "smooth"});
+            }
+        });
+    }
+
+
+
+    //product page
+    //add to cart disabled
+    const sizeSelect = document.getElementById("size");
+    const addButton = document.getElementById("add_to_cart");
+    const addButtonDisabled = document.getElementById("add_to_cart_disabled");
+
+    sizeSelect.addEventListener("change", function(){
+        if (sizeSelect.value !== ""){
+            addButton.style.display = "block";
+            addButtonDisabled.style.display = "none";
         }
     });
-}
-for (let i = 0; i < rightButtons.length; i++) {
-    rightButtons[i].addEventListener("click", () => {
-        if (i === 0) {
-            scrollLIT.scrollBy({left: scrollAm, behavior: "smooth"});
-        } else {
-            scrollTS.scrollBy({left: scrollAm, behavior: "smooth"});
-        }
-    });
-}
-//scrolling product page items
-const scrollProd = document.getElementById('photos_list');
-const scrollReviews = document.getElementById('reviews');
-for (let i = 0; i < leftButtons.length; i++) {
-    leftButtons[i].addEventListener("click", () => {
-        if (i === 0) {
-            scrollProd.scrollBy({left: -scrollAm, behavior: "smooth"});
-        } else {
-            scrollReviews.scrollBy({left: -scrollAm, behavior: "smooth"});
-        }
-    });
-}
-for (let i = 0; i < rightButtons.length; i++) {
-    rightButtons[i].addEventListener("click", () => {
-        if (i === 0) {
-            scrollProd.scrollBy({left: scrollAm, behavior: "smooth"});
-        } else {
-            scrollReviews.scrollBy({left: scrollAm, behavior: "smooth"});
-        }
-    });
-}
-
-
-
-
-
 
 
 
